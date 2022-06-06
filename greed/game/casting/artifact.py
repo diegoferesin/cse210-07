b61b4478e014e0c70aa4769bc7fc3fc29f910e0d
Emma Lund <emma.lund22@gmail.com>
2022-06-05 21:42:37 -0600

    rock or gem class!

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -1,3 +1,18 @@
 from game.casting.actor import Actor
+import random
 
-# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Item(Actor):
+    def __init(self):
+        super().__init__()
+        self._rock_or_gem = ""
+
+    def set_item_type(self):
+        num = random.randint(1,2)
+        if num == 1:
+            self._rock_or_gem = "rock"
+        elif num == 2:
+            self._rock_or_gem = "gem"
+        pass
+
+    def get_item_type(self):
+        return self._rock_or_gem
